f7d54070e03ad51dbdf591b6cfa4ca1c183ed437
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-08-15 16:27:50 +0700

    feat: add redis

---
+++ infrastructures/Redis.go
@@ -0,0 +1,42 @@
+package infrastructures
+
+import (
+	"os"
+
+	redis "github.com/go-redis/redis/v7"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// IRedis interface
+type IRedis interface {
+	Client() *redis.Client
+}
+
+var redisClient *redis.Client
+
+// Redis struct
+type Redis struct{}
+
+// Client return new redis client from given address and password
+func (r *Redis) Client() *redis.Client {
+
+	if redisClient == nil {
+		client := redis.NewClient(&redis.Options{
+			Addr:     viper.GetString("redis.address"),
+			Password: viper.GetString("redis.password"), // no password set
+			DB:       0,                                 // use default DB
+		})
+		_, err := client.Ping().Result()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"code":  5500,
+				"error": err,
+			}).Error(err)
+			os.Exit(1)
+		}
+		redisClient = client
+	}
+
+	return redisClient
+}
