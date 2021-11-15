4f706465735480e10ce7ece68f3f1cd5aa302a94
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-11-15 10:50:33 +0700

    fix: remove db repo sniper

--- repositories/SniperRepository.go
+++ repositories/SniperRepository.go
@@ -10,11 +10,9 @@ import (
 	"net/http"
 	"time"
 
-	dbr "github.com/gocraft/dbr/v2"
 	"github.com/rzknugraha/zorro-mark/helpers"
 	"github.com/rzknugraha/zorro-mark/infrastructures"
 	"github.com/rzknugraha/zorro-mark/models"
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -26,7 +24,6 @@ type ISniperRepository interface {
 
 // SniperRepository is
 type SniperRepository struct {
-	DB    infrastructures.ISQLConnection
 	Redis infrastructures.IRedis
 }
 
@@ -35,22 +32,6 @@ var URLSniper = viper.GetString("sniper.url")
 
 //AppNameSniper app name
 var AppNameSniper = "sniper"
-
-// Tx init a new transaction
-func (r *SniperRepository) Tx() (tx *dbr.Tx, err error) {
-	db := r.DB.EsignWrite()
-	tx, err = db.Begin()
-
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"code":  5500,
-			"error": err,
-			"data":  nil,
-		}).Error("Error begin transaction")
-	}
-
-	return
-}
 
 // LoginMehong store agent type data to database
 func (r *SniperRepository) LoginMehong(ctx context.Context, c models.EncryptedCookies) (response models.ResponseSniper, err error) {
